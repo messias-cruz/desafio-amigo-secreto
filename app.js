2899fc5b98e334cd3d6fc74816cb915b448154bc
messias-cruz <messias.carreira@gmail.com>
2025-01-28 16:00:51 -0300

    array criada para armazenar os nomes

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let amigo = ['José','Maria','João','Bruna','Messias','Laura','Grimaldo','Lídia','Wellington','Elza','Sidney','Almerinda','Willians','Taís','Miguel','Juliana','Gilberto','Amanda','Douglas','Camila','Enzo','Isabela','Matheus','Luana','Guilherme','Mariana','Rafael','Beatriz','Bruno','Larissa'];
+
